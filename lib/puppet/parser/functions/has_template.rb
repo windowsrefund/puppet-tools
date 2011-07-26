65c656e508db4bacfb646dba0ee9078cb8980c5f
windowsrefund <windowsrefund@gmail.com>
2011-07-26 17:13:53 -0400

    first commit

---
+++ lib/puppet/parser/functions/has_template.rb
@@ -0,0 +1,15 @@
+Puppet::Parser::Functions::newfunction(
+  :has_template, 
+  :type => :rvalue,
+  :doc => "Returns the first arg which translates to a valid template.") do |args|
+
+	require 'rubygems'
+  require 'puppet'
+
+  args.delete_if { |a| Puppet::Parser::Files.find_template(a).nil? }
+
+  raise Puppet::ParseError, "No valid template found." if args.empty? 
+  args.first
+  
+end
+
